3f7b0e2b80d1cd5cd83a740ac060cfc87b094211
Gábor Bergmann <gabor.bergmann@incquerylabs.com>
2019-05-17 18:56:24 +0200

    #1 Add experimental Conda packaging
    Signed-off-by: Gábor Bergmann <gabor.bergmann@incquerylabs.com>

---
+++ source/incqueryserver-jupyter/iqs_jupyter/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2019 IncQuery Labs Ltd.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#  http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''
+    IncQuery Server Client Extensions for Jupyter and Python
+
+    Requires [IncQuery Server](https://incquery.io) to operate.
+
+
+Created on 2019-05-17
+
+@author: Gábor Bergmann
+'''
+
+# coding: utf-8
+from __future__ import absolute_import
+
+__version__ = "0.0.1"
+
+from iqs_jupyter.iqs_jupyter import *
